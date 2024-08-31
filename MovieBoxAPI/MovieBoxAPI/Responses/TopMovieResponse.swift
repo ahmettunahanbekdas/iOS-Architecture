4057f4e976509e1807f5182f50d7dd0341b5a6c1
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2024-08-31 14:26:47 +0300

    respons added

---
+++ MovieBoxAPI/MovieBoxAPI/Responses/TopMovieResponse.swift
@@ -0,0 +1,31 @@
+//
+//  TopMovieResponse.swift
+//  MovieBoxAPI
+//
+//  Created by Ahmet Tunahan Bekdaş on 30.08.2024.
+//
+
+import Foundation
+
+public struct TopMovieResponse: Decodable {
+    
+    private enum RootCodingKeys: String, CodingKey {
+        case feed
+    }
+    
+    private enum FeedCodingKeys: String, CodingKey {
+        case results
+    }
+    
+    public let results: [Movie]
+    
+    public init(from  decoder: Decoder) throws {
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        let feedContainer = try rootContainer.nestedContainer(keyedBy: FeedCodingKeys.self, forKey: .feed)
+        self.results = try feedContainer.decode([Movie].self, forKey: .results)
+    }
+}
+
+
+
+
